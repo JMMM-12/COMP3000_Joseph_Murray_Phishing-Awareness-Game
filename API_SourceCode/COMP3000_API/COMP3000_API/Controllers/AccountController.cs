a742c3035271142e6fb828cc90e329dcd49100d0
JMMM-12 <joseph.murray-10@students.plymouth.ac.uk>
2024-10-22 13:20:56 +0100

    Created an ASP.NET Core Web API for API development

---
+++ API_SourceCode/COMP3000_API/COMP3000_API/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP3000_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
