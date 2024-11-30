b966e6e325ac27833ccafc3f783f8ddda223984e
JMMM-12 <joseph.murray-10@students.plymouth.ac.uk>
2024-11-30 12:07:47 +0000

    Replaced the GameStateManager class with a ScriptableObject class

---
+++ Game_SourceCode/Anti_Phisher/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameStateManager", menuName = "ScriptableObjects/GameStateManager")] //Creates a new Asset menu for easier creation of this GameStateManager Asset
+public class GameStateManager : ScriptableObject //Manages the state of the game and its features
+{
+    //Variables that can be retrieved and modified from other scripts
+    public GameState gameState;
+    public EncounterState encounterState;
+    public int EncounterNum;
+    public bool dialogueActive;
+}
+
+
+
+public enum GameState
+{
+    Start,
+    Playing,
+    Paused,
+    Closed,
+    Unknown
+}
+
+
+
+public enum EncounterState
+{
+    Beginning,
+    Middle,
+    Feedback,
+    Inactive,
+    Unknown
+}
