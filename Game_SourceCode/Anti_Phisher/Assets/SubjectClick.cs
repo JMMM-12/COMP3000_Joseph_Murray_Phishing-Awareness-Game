3bfa30a3b0ee75a34239d84e60d2a351f04cff01
JMMM-12 <joseph.murray-10@students.plymouth.ac.uk>
2024-12-06 14:15:58 +0000

    Created a new script that performs selection and deselection logic upon a click on the Subject indicator

---
+++ Game_SourceCode/Anti_Phisher/Assets/SubjectClick.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SubjectClick : MonoBehaviour, IPointerClickHandler //Tracks when the subject indicator is clicked on and changes the selection value accordingly
+{
+    public GameStateManager gameStateManager;
+    public SelectionData selectionData;
+
+    public void OnPointerClick(PointerEventData eventData)
+    { 
+        if (gameStateManager.encounterActive == true) //Checks that the encounter is currently active
+        {
+            if (gameStateManager.encounterState == EncounterState.Indicators) //Checks that the encounter is in the indicators state
+            {
+                if (gameStateManager.emailDisplayed == true && gameStateManager.emailContentsDisplayed == true) //Checks that the email UI elements & contents have been displayed
+                {
+                    if (selectionData.indicatorSelection.subjectSelected == false) //Checks that the subject value is not already selected
+                    {
+                        selectionData.indicatorSelection.subjectSelected = true; //Marks the subject as selected
+                        Debug.Log("Subject text was selected");
+                    }
+                    else
+                    {
+                        selectionData.indicatorSelection.subjectSelected = false; //Marks the subject as unselected
+                        Debug.Log("Subject text was deselected");
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+}
