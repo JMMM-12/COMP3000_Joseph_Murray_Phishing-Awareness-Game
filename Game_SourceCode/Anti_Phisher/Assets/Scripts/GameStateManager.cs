9cee992a15367ccb2d18b513cccee4f8a60c9297
JMMM-12 <joseph.murray-10@students.plymouth.ac.uk>
2024-12-08 14:25:01 +0000

    Modified the Game State Manager to include a feedback state for tracking the individual stages of the feedback phase

--- Game_SourceCode/Anti_Phisher/Assets/Scripts/GameStateManager.cs
+++ Game_SourceCode/Anti_Phisher/Assets/Scripts/GameStateManager.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameStateManager", menuName = "ScriptableObjects/GameStateManager")] //Creates a new Asset menu for easier creation of this GameStateManager Asset
-public class GameStateManager : ScriptableObject //Manages the state of the game and its features
+public class GameStateManager : ScriptableObject //Manages the state of the game and its specific phases
 {
     //Variables that can be retrieved and modified from other scripts
-    public GameState gameState;
-    public DialogueStage dialogueStage;
-    public EncounterState encounterState;
-    public int EncounterNum;
-    public bool dialogueActive;
-    public bool encounterActive;
-    public bool emailDisplayed;
-    public bool emailContentsDisplayed;
-    public bool highlightersReady;
+    public GameState gameState; //High-level game state of the entire game
+    public DialogueStage dialogueStage; //State of the current encounter's dialogue
+    public EncounterState encounterState; //State of the current encounter
+    public FeedbackState feedbackState; //state of the current encounter's feedback phase
+    public int EncounterNum; //Indicates the encounter the game is currently on
+    public bool dialogueActive; //Determines whether the dialogue is active or inactive
+    public bool encounterActive; //Determines whether the encounter is active or inactive
+    public bool emailDisplayed; //Determines whether the email UI elements are active and displayed
+    public bool emailContentsDisplayed; //Determines whether the email contents are displayed
+    public bool highlightersReady; //Determined whether the indicator highlighter images are ready
 }
 
 
@@ -44,4 +45,14 @@ public enum EncounterState
     Response,
     Feedback,
     Unknown
+}
+
+
+
+public enum FeedbackState
+{
+    AnswersCheck,
+    FeedbackDetermine,
+    FeedbackDisplay,
+    Inactive
 }
