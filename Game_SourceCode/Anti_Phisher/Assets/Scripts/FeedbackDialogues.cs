5426d76446df300bb8fe1c4d7bf4844815272e08
JMMM-12 <joseph.murray-10@students.plymouth.ac.uk>
2024-12-10 17:32:57 +0000

    Implemented a ScriptableObject Asset store for the feedback dialogues to use per encounter

---
+++ Game_SourceCode/Anti_Phisher/Assets/Scripts/FeedbackDialogues.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FeedbackDialogues", menuName = "ScriptableObjects/FeedbackDialogues")]
+public class FeedbackDialogues : ScriptableObject
+{
+    public List<FDialogues> feedbackDialogues;
+}
+
+
+[System.Serializable]
+public class FDialogues
+{
+    public string ChipModel;
+    public string FeedbackText;
+}
